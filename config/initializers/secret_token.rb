df241e08f1d59433d62ada9126bb5567acfbc224
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-01-01 18:47:44 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarInventory::Application.config.secret_key_base = '93629c9438f8e9030ec0dac61ab8d52fa4acd5a79b8bacc65a257e06bdca196b88afc1978d5e338bc29e08728916878d7723f8cc15d1d375a4c9d01c2fd1c089'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CarInventory::Application.config.session_store :cookie_store, key: '_Car_Inventory_session'
